d936100828c1a3f78fb0e3fdb6b3e93424f64f8d
Fredrik Johansson <fredrik.johansson@thnx.se>
2023-05-19 13:49:39 +0200

    move create user code to separate page

---
+++ src/app/create-user/page.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { db } from "@/db/db";
+import { UsersTable } from "@/db/schema";
+import { currentUser } from "@clerk/nextjs";
+import { RedirectType } from "next/dist/client/components/redirect";
+import { redirect } from "next/navigation";
+
+async function createUser(data: FormData) {
+  "use server";
+
+  const user = await currentUser();
+  if (!user) throw Error("No clerk user provided at login.");
+  const email = user.emailAddresses[0]?.emailAddress as string | undefined;
+  if (!email) throw Error("No email provided in clerk object");
+  const fullName = data.get("fullName");
+  if (!fullName) throw Error("No name provided in form");
+
+  const users = await db
+    .insert(UsersTable)
+    .values([{ clerkId: user.id, email, fullName: fullName.toString() }])
+    .returning();
+  if (users.length === 0) throw new Error("Failed to create user");
+  console.log("Created user");
+  redirect("/", RedirectType.replace);
+}
+
+export default async function CreateUser() {
+  const user = await currentUser();
+
+  if (!user) throw Error("No clerk user provided at login.");
+
+  return (
+    <div>
+      Using email: {user.emailAddresses[0]?.emailAddress}
+      <form action={createUser}>
+        <Input placeholder="fullName" name="fullName" />
+        <Button type="submit">Create user</Button>
+      </form>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,21 @@
 import { db } from "@/db/db";
-import { NewUser, UsersTable } from "@/db/schema";
+import { UsersTable } from "@/db/schema";
 import { UserButton, auth } from "@clerk/nextjs";
 import { like } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
 export const runtime = "edge";
 
 export default async function Home() {
   const user = auth();
-
-  if (!user.userId) throw new Error("No user id");
+  if (!user.userId) throw Error("No clerk id provided at login.");
 
   const users = await db.select().from(UsersTable).where(like(UsersTable.clerkId, user.userId));
-  let dbUser = users[0];
+  const dbUser = users.at(0);
+
   if (!dbUser) {
-    console.log("no user found, creating one");
-    const newUser: NewUser = {
-      clerkId: user.userId,
-      email: "who@knows.com",
-    };
-    const users = await db.insert(UsersTable).values([newUser]).returning();
-    if (users.length === 0) throw new Error("Failed to create user");
-    console.log("created user");
-    dbUser = users[0];
-  } else {
-    console.log("found user");
+    console.warn(`No user with clerk id ${user.userId} found in db. Redirect to create-user page.}`);
+    redirect("/create-user");
   }
 
   return (
