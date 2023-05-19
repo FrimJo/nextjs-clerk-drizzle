c893a38686a62dc16b7c495642837f3258010900
Fredrik Johansson <fredrik.johansson@thnx.se>
2023-05-19 13:53:20 +0200

    add runtime edge to create-user page

--- src/app/create-user/page.tsx
+++ src/app/create-user/page.tsx
@@ -6,6 +6,8 @@ import { currentUser } from "@clerk/nextjs";
 import { RedirectType } from "next/dist/client/components/redirect";
 import { redirect } from "next/navigation";
 
+export const runtime = "edge";
+
 async function createUser(data: FormData) {
   "use server";
 
